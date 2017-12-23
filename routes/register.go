aa364ff96afde883bfbd17fecdd7f9970d9c9024
Max Obermeier <themomax@icloud.com>
2017-12-23 19:52:58 +0100

    template fixes

--- routes/register.go
+++ routes/register.go
@@ -7,5 +7,12 @@ import (
 )
 
 func Register(ctx *macaron.Context, log *log.Logger) {
+
+	type Config struct {
+		MinYL, MaxYL int
+	}
+
+	ctx.Data["Config"] = Config{1, 12}
+
 	ctx.HTML(200, "register")
 }
