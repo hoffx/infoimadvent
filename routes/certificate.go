600f39a82c783438069e406bfcf18dfdfc30b58b
Thomas Kirz <thomas.kirz@gmail.com>
2018-02-12 11:45:08 +0100

    Fix support for non-English characters in certificate

--- routes/certificate.go
+++ routes/certificate.go
@@ -46,33 +46,34 @@ func GenerateFont() error {
 
 func generateCertificate(file io.Writer, user storage.User, ctx *macaron.Context) error {
 	pdf := gofpdf.New("P", "mm", "A4", fontDir)
+	utf8tr := pdf.UnicodeTranslatorFromDescriptor("cp1252")
 	pdf.AddFont("Zilla Slab", "", "ZillaSlab-Regular.json")
 	pdf.AddPage()
 
 	pdf.Ln(39)
 	pdf.SetFont("Zilla Slab", "", 109)
 	pdf.SetTextColor(196, 187, 69)
-	pdf.CellFormat(0, 32, ctx.Tr("Certificate"), "", 1, "C", false, 0, "")
+	pdf.CellFormat(0, 32, utf8tr(ctx.Tr("certificate")), "", 1, "C", false, 0, "")
 
 	pdf.Ln(39)
-	pdf.SetFont("Zilla Slab", "", 38)
+	pdf.SetFont("Zilla Slab", "", 35)
 	pdf.SetTextColor(34, 34, 34)
-	pdf.CellFormat(0, 10, ctx.Tr("awarded_to"), "", 1, "C", false, 0, "")
+	pdf.CellFormat(0, 10, utf8tr(ctx.Tr("awarded_to")), "", 1, "C", false, 0, "")
 
 	pdf.Ln(25)
 	pdf.SetFont("Zilla Slab", "", 38)
 	pdf.SetTextColor(196, 187, 69)
-	pdf.CellFormat(0, 10, user.Email, "", 1, "C", false, 0, "")
+	pdf.CellFormat(0, 10, utf8tr(user.Email), "", 1, "C", false, 0, "")
 
 	scoreStr := ctx.Tr("certificate_score_pre") + " " + strconv.Itoa(user.Score) + " " + ctx.Tr("certificate_score_post")
 	pdf.Ln(25)
-	pdf.SetFont("Zilla Slab", "", 38)
+	pdf.SetFont("Zilla Slab", "", 35)
 	pdf.SetTextColor(34, 34, 34)
-	pdf.CellFormat(0, 10, scoreStr, "", 1, "C", false, 0, "")
+	pdf.CellFormat(0, 10, utf8tr(scoreStr), "", 1, "C", false, 0, "")
 
 	pdf.Ln(50)
 	pdf.SetFont("Zilla Slab", "", 27)
-	pdf.CellFormat(0, 10, ctx.Tr("service"), "", 1, "C", false, 0, "")
+	pdf.CellFormat(0, 10, utf8tr(ctx.Tr("service")), "", 1, "C", false, 0, "")
 
 	err := pdf.Output(file)
 	pdf.Close()
